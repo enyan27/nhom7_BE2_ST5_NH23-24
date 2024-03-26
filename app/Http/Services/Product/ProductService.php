26546c809f78f1bad92f3e37335801fe02a05dcb
enyan27 <truongdat2325@gmail.com>
2024-03-26 15:19:35 +0700

    create requests and update services #2

--- app/Http/Services/Product/ProductService.php
+++ app/Http/Services/Product/ProductService.php
@@ -3,36 +3,39 @@
 namespace App\Http\Services\Product;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\ProductDetail;
 use App\Models\Brand;
 use App\Models\Category;
-use Illuminate\Support\Facades\Auth;
-use DB;
-use Illuminate\Http\Request;
 
-
-class ProductService{
-
-    public function getAll(){
+class ProductService 
+{
+    public function getAll() {
 
         return Product::orderByDesc('id')->paginate(10);
     }
-    private function uploadImg(Request $request, $fieldImg, $attr){
+
+    private function uploadImg(Request $request, $fieldImg, $attr) {
+
         $product_slug = Str::of($request->input('productname'))->slug('-');
 
-        if($request->hasFile($fieldImg)){
+        if($request->hasFile($fieldImg)) {
+            
             $original_name = $request->file($fieldImg)->getClientoriginalName();
             $ext = $request->file($fieldImg)->extension();
             $new_name = $product_slug . '-' . md5($original_name) . '.' .$ext;
+
             $path = 'uploads/product';
             $request->file($fieldImg)->storeAs('public/' . $path , $new_name);
+
             return $request->merge([$attr => 'storage/'. $path . '/' . $new_name ]);
         }
-        
     }
     
-    public function store($request){
+    public function store($request) {
 
         $this->uploadImg($request, 'imageProduct_1','image_1');
         $this->uploadImg($request, 'imageProduct_2', 'image_2');
@@ -46,10 +49,9 @@ class ProductService{
         $product['created_by'] = Auth::user()->id;
 
         return Product::create($product);
-          
     }
 
-    public function update($request, $product_id){
+    public function update($request, $product_id) {
         
         $this->uploadImg($request, 'imageProduct_1','image_1');
         $this->uploadImg($request, 'imageProduct_2', 'image_2');
@@ -58,13 +60,15 @@ class ProductService{
         $data['slug'] = Str::of($request->input('productname'))->slug('-');
 
         $this->find($product_id)->update($data);
-
     }
 
-    public function destroy($product_id){
+    public function destroy($product_id) {
+
         $productDetails = ProductDetail::where('product_id', $product_id)->get();
-       if($this->find($product_id)->delete()){
-            foreach($productDetails as $productDetail){
+
+        if ($this->find($product_id)->delete()) {
+
+            foreach ($productDetails as $productDetail) {
                 $productDetail->delete();
             }
             return true;
@@ -72,15 +76,18 @@ class ProductService{
         return false;
     }
 
-    public function find($product_id){
+    public function find($product_id) {
+
         return Product::Find($product_id);
     }
 
-    public function updateQuantity($product_id){
+    public function updateQuantity($product_id) {
+
         Product::updateQty($product_id);
     }
 
-    public function getProductBySlug($slug){
+    public function getProductBySlug($slug) {
+
         $product =  Product::where('slug', $slug)->first();
         $product->view_count ++;
         $product->save();
@@ -88,14 +95,18 @@ class ProductService{
         return $product;
     }
     
-    public function getRelatedProduct($catgegory_id){
-        return Product::where('category_id', $catgegory_id)->orderByDesc('id')->limit(8)->get();
+    public function getRelatedProduct($catgegory_id) {
+
+        return Product::where('category_id', $catgegory_id)
+                                        ->orderByDesc('id')
+                                        ->limit(8)->get();
     }
 
-    public function getProductByCategory($category_slug, $request){
+    public function getProductByCategory($category_slug, $request) {
+
         $products = Product::join('categories','categories.id', '=', 'products.category_id')
-                        ->where('categories.slug', $category_slug)
-                        ->select(['products.*']);
+                                        ->where('categories.slug', $category_slug)
+                                        ->select(['products.*']);
         
         $products = $this->filter($products, $request);
         $products = $this->sortAndPagination($products, $request);
@@ -103,19 +114,19 @@ class ProductService{
         return $products;
     }
 
-    public function getProductOnIndex($request){
+    public function getProductOnIndex($request) {
         
         $search = $request->search ?? '';
         $cate_search = $request->cate_search ?? null;
         
-        if($cate_search == null){
+        if($cate_search == null) {
             $products = Product::where('productname', 'like', '%'.$search.'%');
         }
-        else{
+        else {
             $products = Product::join('categories','categories.id', '=','products.category_id')
-                            ->where('productname', 'like', '%'.$search.'%')
-                            ->where('categories.slug', $cate_search)
-                            ->select(['products.*']);
+                                        ->where('productname', 'like', '%'.$search.'%')
+                                        ->where('categories.slug', $cate_search)
+                                        ->select(['products.*']);
         }
 
         $products = $this->filter($products, $request);
@@ -124,7 +135,7 @@ class ProductService{
         return $products;
     }
     
-    private function sortAndPagination($products, Request $request){
+    private function sortAndPagination($products, Request $request) {
         $perPage = $request->show ?? 12;
         $sortBy = $request->sort_by ?? 'latest';
         
@@ -147,7 +158,8 @@ class ProductService{
             case 'price-desending':
                 $products = $products->orderByDesc('price_sale');
                 break;
-            default: $products = $products->orderByDesc('id');
+            default:
+                $products = $products->orderByDesc('id');
         }
 
         $products = $products->paginate($perPage);
@@ -161,9 +173,10 @@ class ProductService{
         //Brand
         $brands = $request->brand ?? [];    
         $brand_names = array_keys($brands);
-        if($brand_names != null){
-            $products = $products->whereHas('Brand', function($query) use ($brand_names){
-                                            return $query->whereIn('brandname', $brand_names);
+
+        if($brand_names != null) {
+            $products = $products->whereHas('Brand', function($query) use ($brand_names) {
+                return $query->whereIn('brandname', $brand_names);
             });
         }    
 
@@ -177,9 +190,9 @@ class ProductService{
         $sizes = $request->size ?? [];
         $arr_sizes = array_keys($sizes);
 
-        $products = $arr_sizes != null ? $products->whereHas('ProductDetail', function($query) use ($arr_sizes){
-                                                            return $query->whereIn('size', $arr_sizes)
-                                                                        ->where('quantity', '>', '0');
+        $products = $arr_sizes != null ? $products->whereHas('ProductDetail', function($query) use ($arr_sizes) {
+            return $query->whereIn('size', $arr_sizes)
+                            ->where('quantity', '>', '0');
         }) : $products;
 
         return $products;
--- app/Http/Services/ProductDetail/ProductDetailService.php
+++ app/Http/Services/ProductDetail/ProductDetailService.php
@@ -9,27 +9,29 @@ use Auth;
 use Illuminate\Support\Facades\Hash;
 use Request;
 
+class ProductDetailService
+{
+    private function uploadImg($request, $fieldImg, $product_id, $attr) {
 
-class ProductDetailService{
-
-    private function uploadImg($request, $fieldImg, $product_id, $attr){
         $product_slug = Product::find($product_id)->slug;
         
-        if($request->hasFile($fieldImg)){
+        if($request->hasFile($fieldImg)) {
+
             $original_name = $request->file($fieldImg)->getClientoriginalName();
             $ext = $request->file($fieldImg)->extension();
             $new_name = $product_slug . '-' . md5($original_name) . '.' .$ext;
+
             $path = 'uploads/product';
             $request->file($fieldImg)->storeAs('public/' . $path , $new_name);
+
             return $request->merge([$attr => 'storage/'. $path . '/' . $new_name ]);
         }
-
     }
 
-    public function store($request, $product_id){
+    public function store($request, $product_id) {
         
-        $this->uploadImg($request, 'imageProduct_1', $product_id,'colorImg_1');
-        $this->uploadImg($request, 'imageProduct_2', $product_id,'colorImg_2');
+        $this->uploadImg($request, 'imageProduct_1', $product_id, 'colorImg_1');
+        $this->uploadImg($request, 'imageProduct_2', $product_id, 'colorImg_2');
 
         $productDetail = $request->all();
         $productDetail['product_id'] = $product_id;
@@ -38,24 +40,25 @@ class ProductDetailService{
 
     public function update($request, $product_id, $productDetail_id){
         
-        $this->uploadImg($request, 'imageProduct_1', $product_id,'colorImg_1');
-        $this->uploadImg($request, 'imageProduct_2', $product_id,'colorImg_2');
+        $this->uploadImg($request, 'imageProduct_1', $product_id, 'colorImg_1');
+        $this->uploadImg($request, 'imageProduct_2', $product_id, 'colorImg_2');
 
         $data = $request->all();
         ProductDetail::Find($productDetail_id)->update($data);
-            
     }
 
-    public function destroy($productDetail_id){
+    public function destroy($productDetail_id) {
+
         $productDetail = ProductDetail::Find($productDetail_id);
 
-        if($productDetail){
-            return ProductDetail::where('id',$productDetail_id)->delete();
+        if($productDetail) {
+            return ProductDetail::where('id', $productDetail_id)->delete();
         }
     }
 
-    public function find($product_id, $productDetail_id){
+    public function find($product_id, $productDetail_id) {
         
-        return ProductDetail::where('product_id', $product_id)->Where('id', $productDetail_id)->first();
+        return ProductDetail::where('product_id', $product_id)
+                                ->Where('id', $productDetail_id)->first();
     }
 }
